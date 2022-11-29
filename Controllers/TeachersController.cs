173ecf5c1fbbc506353e3c8590fec42f821dc6d2
Denislava <ivanova.denislava@gmail.com>
2022-11-29 16:12:53 +0200

    TeacherAddFinalized

--- Controllers/TeachersController.cs
+++ Controllers/TeachersController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Kindergarten2.Controllers
 {
 	using Kindergarten2.Data;
+	using Kindergarten2.Data.Models;
 	using Kindergarten2.Models.Teachers;
 	using Microsoft.AspNetCore.Mvc;
 	using System.Collections.Generic;
@@ -20,12 +21,31 @@
 		[HttpPost]
 		public IActionResult Add(AddTeacherFormModel teacher)
 		{
+			if (!this.data.Groups.Any(g => g.Id == teacher.GroupId))
+			{
+				this.ModelState.AddModelError(nameof(teacher.GroupId), "Group does not exist.");
+			}
+
 			if (!ModelState.IsValid)
 			{
 				teacher.Groups = this.GetTeacherGroups();
 
 				return View(teacher);
 			}
+
+			var teacherData = new Teacher
+			{
+				GroupId = teacher.GroupId,
+				FirstName = teacher.FirstName,
+				LastName = teacher.LastName,
+				Experience = teacher.Experience,
+				Specialization = teacher.Specialization
+
+			};
+
+			this.data.Teachers.Add(teacherData);
+
+			this.data.SaveChanges();
 
 			return RedirectToAction("Index", "Home");
 		}
